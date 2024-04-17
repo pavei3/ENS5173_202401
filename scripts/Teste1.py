d8ba1f2a9f8be7f19145129ef8e18ac8f55d418f
pavei3 <ufscpavei@gmail.com>
2024-04-17 08:20:20 -0300

    h

---
+++ scripts/Teste1.py
@@ -0,0 +1,15 @@
+lista= [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]]
+listax=[]
+len(lista)
+numrows = len(lista)
+numcols = len(lista[0])
+listaidealx=[1,2,3,4,5,6,7,8,9]
+listaidealy=[1,1,1,2,2,2,3,3,3]
+o=range(0,10)
+for n in o:
+    print(n)
+
+for linha in enumerate(lista):
+    print(linha)
